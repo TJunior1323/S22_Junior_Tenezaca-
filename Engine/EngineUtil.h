71c6f3517590e0e6b5b22fa4772c90a2ad460554
Junior <tenezacajunior@gmail.com>
2022-03-22 13:16:56 -0400

    updated CMake files

--- Engine/EngineUtil.h
+++ Engine/EngineUtil.h
@@ -2,10 +2,14 @@
 
 #include "pch.h"
 
-#ifdef ENGINE_LIB
-	#define ENGINE_API __declspec(dllexport)
+#ifdef ENGINE_WINDOWS
+	#ifdef ENGINE_LIB
+		#define ENGINE_API __declspec(dllexport)
+	#else
+		#define	ENGINE_API __declspec(dllexport)
+	#endif
 #else
-	#define	ENGINE_API __declspec(dllexport)
+	#define ENGINE_API
 #endif
 
 #ifdef ENGINE_DEBUG
